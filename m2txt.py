43a7f1912f6ec913a4d0fd5924496b0bf0d02b5f
Mario Garcia <mayitzin@gmail.com>
2016-03-27 15:21:21 +0200

    Print line number next to each line

--- m2txt.py
+++ m2txt.py
@@ -9,11 +9,21 @@ www.mayitzin.com
 """
 
 fileName = "chordist.m"
+block = False
+indices = []
 
 with open(fileName, 'r') as inputFile:
     text = inputFile.readlines()
 
 for line in text:
+    index = []
     if line.startswith("%"):
-    	print line.strip()
+        if block is False: index.append(text.index(line))
+        block = True
+        print text.index(line),":\t",line.strip()
+    else:
+        if block is True: index.append(text.index(line))
+        block = False
+    indices.append(index)
 
+# print indices
