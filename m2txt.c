77220c50d7d471263620c438c29a835fad263a1d
Mario Garcia <mayitzin@gmail.com>
2016-04-26 15:05:39 +0200

    Add M-file parser in C

---
+++ m2txt.c
@@ -0,0 +1,16 @@
+/**
+ * @file m2txt.c
+ * @brief M-file Parser
+ *
+ * Parser of M-files to get the raw text from comments and classify them.
+ *
+ * @author Mario Garcia (www.mayitzin.com)
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	printf("This is a simple M-file reader\n");
+
+	return (0);
+}
