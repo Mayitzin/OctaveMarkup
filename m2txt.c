422f969e4cfc69c2375911bda18164e6fe7d20a0
Mario Garcia <mayitzin@gmail.com>
2016-04-26 15:28:47 +0200

    Handle missing file

--- m2txt.c
+++ m2txt.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>         // strlen, strcat, strcpy, strcmp, strtok, atof
+#include <errno.h>
 
 int main(int argc, char *argv[]){
     if (argc<2){
@@ -21,7 +22,14 @@ int main(int argc, char *argv[]){
     char inputFile[50];
     strcpy(inputFile, argv[1]);
 
-    printf("Input File: %s\n", inputFile);
+	int errnum;
+    inFile  = fopen(inputFile, "r");
+    if(inFile==NULL){
+    	errnum = errno;
+        fprintf(stderr, "%s\n", strerror(errnum));
+        exit(EXIT_FAILURE);
+    }
+    fclose(inFile);
 
     return (0);
 }
