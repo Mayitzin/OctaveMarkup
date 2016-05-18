93d45ecc448b9c3c1b6c9d7dd190923c9736504b
Mario Garcia <mayitzin@gmail.com>
2016-05-19 01:57:49 +0200

    Add basic functions to build HTML file

--- buildHTML.py
+++ buildHTML.py
@@ -8,19 +8,43 @@ www.mayitzin.com
 import sys
 
 def addTitle(title):
-	string = "<title>"+title+"</title>"
+    string = "<title>"+title+"</title>\n"
+    return string
+
+def addHead(title):
+	string = "<head>\n"+addTitle(title)+"</head>\n"
+	return string
+
+def addParagraph(contents):
+	string = "<p>"+contents+"</p>\n"
+	return string
+
+def addBody(contents):
+	string = "<body>\n"+addParagraph(contents)+"</body>\n"
+	return string
+
+def buildHTML(contentHead, contentBody):
+	openingTag = "<html>"
+	closingTag = "</html>"
+	string = openingTag+"\n"+contentHead+contentBody+closingTag
 	return string
 
 
 fileName = "chordist.m"
-output = fileName[:-2]+".html"
+outFile = fileName[:-2]+".html"
 
 
 # Read extra parameters
 if len(sys.argv) >= 2:
     fileName = sys.argv[1]
-    output = fileName
+    outFile = fileName
     if fileName[-4:]!="html":
-    	output += ".html"
+        outFile += ".html"
 
-print output
+print outFile+":\n"
+
+contentHead = addHead("My Title")
+contentBody = addBody("A standard Text")
+contentHTML = buildHTML(contentHead, contentBody)
+
+print contentHTML
